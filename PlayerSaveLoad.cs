daab536d13d3ef8391bc62a31d6904f7eedb9a38
Diny Syarifah Sany <dsy.sany@gmail.com>
2025-06-02 09:42:19 +0700

    Add files via upload

---
+++ PlayerSaveLoad.cs
@@ -0,0 +1,62 @@
+//------------------ATTACH SCRIPT INI WAJIB DI SI PLAYER-------------//
+//------------------JANGAN LUPA APPLY OVERRIDES PREFABSNYA-----------//
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class PlayerData
+{
+    public float x, y, z;
+}
+
+public class PlayerSaveLoad : MonoBehaviour
+{
+    private string filePath;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        filePath = Application.persistentDataPath + "/player_position.json";
+        playerTransform = this.transform;
+
+        LoadPlayerPosition();
+    }
+
+    void Update()
+    {
+        SavePlayerPosition();
+        LoadPlayerPosition();
+    }
+
+    public void SavePlayerPosition()
+    {
+        PlayerData data = new PlayerData();
+        data.x = playerTransform.position.x;
+        data.y = playerTransform.position.y;
+        data.z = playerTransform.position.z;
+
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(filePath, json);
+        Debug.Log("Posisi disimpan ke: " + filePath);
+        Debug.Log("Lokasi file: " + Application.persistentDataPath);
+
+    }
+
+    public void LoadPlayerPosition()
+    {
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            PlayerData data = JsonUtility.FromJson<PlayerData>(json);
+
+            Vector3 pos = new Vector3(data.x, data.y, data.z);
+            playerTransform.position = pos;
+
+            Debug.Log("Posisi dimuat: " + pos);
+        }
+        else
+        {
+            Debug.Log("File posisi tidak ditemukan.");
+        }
+    }
+}
